698c6ee5be68bd9cf8e8657e72c2588edf7c710e
Jörg Kantel <joerg@kantel.de>
2020-04-08 19:42:08 +0200

    Create filter.py

---
+++ sources/pillow/filter.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageFilter
+import os
+
+R, G, B = 0, 1, 2
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(file_path)
+
+im_path = os.path.join("images", "sechseck.jpg")
+im = Image.open(im_path)
+
+
+# Mögliche vordefinierte Filter sind BLUR, CONTOUR, DETAIL, EDGE_ENHANCED,
+# EDGE_ENHANCED_MORE, EMBOS, FIND_EDGES, SHARPEN, SMOOTH, SMOOTH_MORE
+filter_im = im.filter(ImageFilter.CONTOUR)
+
+filter_im.show()
