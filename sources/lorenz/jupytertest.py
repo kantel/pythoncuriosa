133df2aaabb2468a90728d4e221c1e85587d012e
Jörg Kantel <joerg@kantel.de>
2020-05-06 17:54:25 +0200

    Create jupytertest.py

---
+++ sources/lorenz/jupytertest.py
@@ -0,0 +1,17 @@
+# %%
+msg = "Hello World"
+print(msg)
+
+# %%
+msg = "Hallo Jörg"
+print(msg)
+
+# %%
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
+
+# %%
