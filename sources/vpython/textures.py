1a26124a2e31482a712ff69f9115d9865ddcb435
Jörg Kantel <joerg@kantel.de>
2021-04-17 20:54:04 +0200

    Create textures.py

---
+++ sources/vpython/textures.py
@@ -0,0 +1,27 @@
+# Textures
+# nach Hans-Bernhard Woyand: Python für Ingebieure und Naturwissenschaftler, S. 235
+
+from vpython import *
+
+WIDTH = 400
+HEIGHT = 400
+
+scene1 = canvas(align = "left", width = WIDTH, height = HEIGHT)
+scene1.title = "<hr />\t\t\t\t<b>Holzklotz</b>\t\t\t\t\t\t\t\t\t<b>Erde</b><hr /><p>"
+scene1.background = color.white
+
+scene1.select()
+p1 = vector(0, 0, 0)
+a1 = vector(0, 1, 1)
+a = box(pos = p1,  axis = a1, texture = {"file":textures.wood})
+
+scene2 = canvas(align = "left", width = WIDTH, height = HEIGHT)
+scene2.background = color.black
+
+scene2.select()
+p2 = vector(0, 0, 0)
+a2 = vector(0, 1, 0)
+b = sphere(pos = p2, texture = {"file":textures.earth})
+while True:
+    b.rotate(angle = pi/120, axis = a2)
+    rate(12)
