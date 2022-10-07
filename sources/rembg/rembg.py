d6a3a3d10acac109fe30ade727729d07d2fce329
Jörg Kantel <joerg@kantel.de>
2022-10-07 21:01:05 +0200

    Rembg (erfolglos) getestet

---
+++ sources/rembg/rembg.py
@@ -0,0 +1,13 @@
+import rembg
+from PIL import Image
+import os
+
+DATAPATH = os.path.join(os.getcwd(), "images")
+
+input_path = os.path.join(DATAPATH, "me2022.jpg")
+output_path = os.path.join(DATAPATH, "output.png")
+
+input = Image.open(input_path)
+output = rembg.remove(input)
+
+output.save(output_path)
