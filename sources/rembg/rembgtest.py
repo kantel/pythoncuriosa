6646fd0b9f98f4807b2450adb8bad0cea7a78615
Jörg Kantel <1219909+kantel@users.noreply.github.com>
2024-02-12 21:04:49 +0100

    Assets added

--- sources/rembg/rembgtest.py
+++ sources/rembg/rembgtest.py
@@ -1,13 +1,30 @@
-from rembg import remove
+import rembg
+import numpy as np
 from PIL import Image
 import os
 
 DATAPATH = os.path.join(os.getcwd(), "images")
 
-input_path = os.path.join(DATAPATH, "me2022.jpg")
-output_path = os.path.join(DATAPATH, "output.png")
+input_path = os.path.join(DATAPATH, "whiterabbit03_02.png")
+output_path = os.path.join(DATAPATH, "whiterabbit03_02_output.png")
 
-input = Image.open(input_path)
-output = remove(input)
+# Load the input image
+input_image = Image.open(input_path)
 
-output.save(output_path)
+# Convert the input image to a numpy array
+input_array = np.array(input_image)
+
+# Apply background removal using rembg
+output_array = rembg.remove(input_array)
+
+# Create a PIL Image from the output array
+output_image = Image.fromarray(output_array)
+
+# Save the output image
+output_image.save(output_path)
+result = Image.new('RGBA', (input_image.width * 2, input_image.height))
+result.paste(input_image, (0, 0))
+result.paste(output_image, (input_image.width, 0))
+result.show()
+
+print("I did it, Babe!")
