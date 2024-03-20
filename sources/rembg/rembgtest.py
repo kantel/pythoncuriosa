59adffbb5d17476fbccba20126e2348ea4bae624
Jörg Kantel <1219909+kantel@users.noreply.github.com>
2024-03-20 20:46:09 +0100

    Update rembgtest.py

--- sources/rembg/rembgtest.py
+++ sources/rembg/rembgtest.py
@@ -5,8 +5,8 @@ import os
 
 DATAPATH = os.path.join(os.getcwd(), "images")
 
-input_path = os.path.join(DATAPATH, "nakednymph00_talking02.png")
-output_path = os.path.join(DATAPATH, "nakednymph00_talking02_output.png")
+input_path = os.path.join(DATAPATH, "nymphs09.png")
+output_path = os.path.join(DATAPATH, "nymphs09_output.png")
 
 # Load the input image
 input_image = Image.open(input_path)
